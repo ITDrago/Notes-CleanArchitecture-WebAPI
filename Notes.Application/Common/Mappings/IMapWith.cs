35cdf7dd0f926c8c8b1c7fa4f2c8133336a78ae1
ITDrago <cimonyanmartin@gmail.com>
2023-11-19 15:58:51 +0300

    The AutoMapper library has been installed and configured, the necessary interfaces and classes have been created.

---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+using System;
+
+
+namespace Notes.Application.Common.Mappings
+{
+    public interface IMapWith<T>
+    {
+        void Mapping(Profile profile) =>
+            profile.CreateMap(typeof(T), GetType());
+    }
+}

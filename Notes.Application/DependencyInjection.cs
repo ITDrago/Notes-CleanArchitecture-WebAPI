91a288612088c2b63bc21273dfa1f8ad6d723a35
ITDrago <cimonyanmartin@gmail.com>
2023-11-24 20:29:46 +0300

    Added and implemented dependency injection

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Reflection;
+
+namespace Notes.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(
+           this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
